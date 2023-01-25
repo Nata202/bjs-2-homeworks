aa17a6a7d22fed8507296183a3841c21b95aa2c5
Nata Potato <natalyagerusova@mail.ru>
2023-01-26 00:09:56 +0300

    Третье задание реализовано

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,11 @@
 function compareArrays(arr1, arr2) {
-  
+    for (let i = 0; i<arr1.length; i++){
+        for (let j = 0; j < arr2.length; j++){
+            console.log(compareArray.every(compareArray => arr1[i] === arr2[j]));
+        }
+    }
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+    let result = users.filter(people => gender === "женский").reduce((acc, item)=> (acc + item) / people.age.length , 0);
 }
