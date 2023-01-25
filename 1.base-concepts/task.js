20f0984f44bffbea7713a078de8ef181998819aa
Nata Potato <natalyagerusova@mail.ru>
2023-01-25 23:06:41 +0300

    Реализовано первое задание

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,23 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
+  let discriminant = (Math.pow(b, 2)) - (4 * a * c);
+  if (discriminant === 0) {
+    arr = [`x = ${-b / (2 * a)}`];
+  } else if (discriminant > 0) {
+    arr = [`x1 = ${(-b + Math.sqrt(d)) / (2 * a)}, x2 =  ${(-b - Math.sqrt(d)) / (2 * a)}`];
+  } else {
+    return arr
+  }
   
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+   let interestRate = percent / 100 / 12;
+    let credit = amount - contribution;
+    let paymentPerMonth = credit * (interestRate + (interestRate / (Math.pow(1 + interestRate) ** countMonths) - 1));
+    let sum = paymentPerMonth * countMonths - contribution;
+
+    return sum
 }
