af8fe1f9ab30e936399b7d4618bdfff8034c35d0
Nata Potato <natalyagerusova@mail.ru>
2023-02-11 18:39:33 +0300

    3 homework changed

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,9 +1,10 @@
 function compareArrays(arr1, arr2) {
-    arr1.every((arr) => arr1.length === arr2.length && arr1[0, 1, 2] === arr2[0, 1, 2])
-}
+    let result1 = arr1.length === arr2.length && arr1.every((compareArrays) =>  [...arr1] === [...arr2]);
+    return result1;
+ }
 
 function getUsersNamesInAgeRange(users, gender) {
-    let result = users.filter(people => people.gender === "женский").map(people => people.age).reduce((acc, item) =>  item / users.length , 0);
+    let result2 = users.filter(user => gender).map(user => people.age).reduce((acc, item) =>  item / acc.length , 0);
 
-    return result;
+    return result2;
 }
