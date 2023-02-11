ec820e88a60ce0ed0b44182d660030c11eb7fdbd
Nata Potato <natalyagerusova@mail.ru>
2023-02-11 16:35:27 +0300

    1 homework changed

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,19 +1,29 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
+  let x1 = 0;
+  let x2 = 0;
   let discriminant = (Math.pow(b, 2)) - (4 * a * c);
   if (discriminant === 0) {
-    arr = [`x = ${-b / (2 * a)}`];
+    x1 = -b / (2 * a);
+    arr.push(x1)
   } else if (discriminant > 0) {
-    arr = [`x1 = ${(-b + Math.sqrt(d)) / (2 * a)}, x2 =  ${(-b - Math.sqrt(d)) / (2 * a)}`];
-  } else {
-    return arr
-  }
-  
-  return arr;
+    x1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+    x2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+    arr.push(x1,x2);
+  } 
+     return Math.floor(arr);
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
+if (typeof(percent) === "string" || typeof(contribution) === "string" || typeof(amount) === "string" || typeof(countMonths) === "string") {
+  Number(percent);
+  Number(contribution);
+  Number(amount);
+  Number(countMonths);
+} else {
+  return false;
+}
    let interestRate = percent / 100 / 12;
     let credit = amount - contribution;
     let paymentPerMonth = credit * (interestRate + (interestRate / (Math.pow(1 + interestRate) ** countMonths) - 1));
