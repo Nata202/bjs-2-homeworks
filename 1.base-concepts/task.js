ced2a3811d09cc6fca3649b7722d5b874b07d2c6
Nata Potato <natalyagerusova@mail.ru>
2023-02-25 21:54:26 +0300

    1.base-concepts changed

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -30,7 +30,7 @@ if (isNaN(percent) || isNaN(contribution) || isNaN(amount) || isNaN(countMonths)
    let interestRate = percent / 100 / 12;
     let credit = amount - contribution;
     let paymentPerMonth = credit * (interestRate + (interestRate / (Math.pow(1 + interestRate) ** countMonths) - 1));
-    let sum = paymentPerMonth * countMonths - contribution;
+    let sum = paymentPerMonth * countMonths;
 
     return Number(sum.toFixed(2))
 }
