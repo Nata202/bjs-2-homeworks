47be48a905be769050f9ae6f127b917a880f17ac
Nata Potato <natalyagerusova@mail.ru>
2023-03-05 22:10:22 +0300

    arrays changed

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,10 +1,10 @@
 function compareArrays(arr1, arr2) {
-    let result1 = arr1.length === arr2.length && arr1.every((compareArrays) =>  [...arr1] === [...arr2]);
+    let result1 = arr1.length === arr2.length && arr1.every((item, index) =>  item === arr2[index]);
     return result1;
  }
 
 function getUsersNamesInAgeRange(users, gender) {
-    let result2 = users.filter(user => gender).map(user => people.age).reduce((acc, item) =>  item / acc.length , 0);
+    let result2 = users.filter(user => user.gender === gender).map(user => user.age).reduce((average, userAge, index, array) =>  average + userAge / array.length, 0);
 
     return result2;
 }
