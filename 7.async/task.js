41bb9001b2915623ae710782373f89bdbc60b94b
Nata Potato <natalyagerusova@mail.ru>
2023-03-12 21:20:54 +0300

    7.async changed

--- 7.async/task.js
+++ 7.async/task.js
@@ -6,53 +6,60 @@ class AlarmClock {
     }
 
     addClock (time, callback) {
-        const hour = +(time[0] + time[1]);
-        const minute = +(time[2] + time[3]);
-        if (time === undefined || callback === undefined){
+        time = new Date().toLocaleTimeString("ru-Ru", {
+            hour: "2-digit",
+            minute: "2-digit",
+          });
+        if (!time || !callback){
             throw new Error ('Отсутствуют обязательные аргументы');
         } else if (this.alarmCollection.find(clock => clock.time === time)) {
             console.warn('Уже присутствует звонок на это же время');
         }
-        return this.alarmCollection.push(Object({callback: callback, time: time, canCall: true}));
+        return this.alarmCollection.push(new Object({callback: callback, time: time, canCall: true}));
 
     }
 
     removeClock(time) {
-        this.call = this.alarmCollection.filter(clock => clock.time === time);
-        delete this.call;
+        this.alarmCollection = this.alarmCollection.filter(clock => clock.time !== time);
+        delete this.alarmCollection;
     }
 
     getCurrentFormattedTime () {
-        return new Date().toTimeString();
+        return new Date().toLocaleTimeString("ru-Ru", {
+            hour: "2-digit",
+            minute: "2-digit",
+          });
     }
 
     start () {
-        if (this.intervalId !== undefined){
+        if (this.intervalId !== null){
             return;
         }
 
-    this.intervalId = setInterval(this.alarmCollection.forEach(clock => clock.time === time), 1000);
+    this.intervalId = setInterval(() => this.Object.forEach({
+        callback: callback, 
+        time: getCurrentFormattedTime(), 
+        canCall: true,
+    }), 1000);
+
     if (this.intervalId === true){
         this.canCall = false;
-        return this.callback;
+        return this.callback();
     }
     }
 
     stop () {
-        function clearInterval(){
-            this.intervalId = null;
-        }
-        return clearInterval();
+        clearInterval(intervalId);
+        this.intervalId = null;
     }
 
     resetAllCalls () {
-        this.alarmCollection.forEach(canCall => this.canCall === true)
+        this.alarmCollection.forEach(() => this.canCall === true)
 
     }
 
     clearAlarms () {
         this.stop();
-        alarmCollection = [];
-        
+        this.alarmCollection = [];        
     }
 }
